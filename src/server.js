00add1a578fe7210bbe4b9635fefb94b3eb56262
João Gabriel Ramos <jgbvramos@id.uff.br>
2024-08-06 21:03:59 -0300

    aula 03: saída no terminal e no localhost:3000/

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.get('/', (req,res)=>{
     res.send('Hello, World, Node.Js!');
 })
 
-app.listen(3333, ()=>{
-    console.log("Nosso servidor está rodando");
+app.listen(port, ()=>{
+    console.log("Nosso servidor está rodando na porta", port);
 })
