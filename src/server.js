e5e649ee285a3ffd6b87499ee53c894da2876c35
João Gabriel Ramos <jgbvramos@id.uff.br>
2024-08-08 20:18:59 -0300

    aula6: baixando swagger: localhost:3333/api-docs/

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+
 const { v4:uuidv4 } = require('uuid');
+
 const app = express();
 
 const router = require('./routes/index');
+const swaggerFile = require('./swagger.json');
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
